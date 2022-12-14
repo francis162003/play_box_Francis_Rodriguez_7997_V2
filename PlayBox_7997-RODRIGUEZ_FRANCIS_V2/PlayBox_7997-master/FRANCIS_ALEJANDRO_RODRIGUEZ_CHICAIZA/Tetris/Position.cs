80d7cd9640ba53e17f4b9f25f1c3684ed277a56a
francis162003 <119018018+francis162003@users.noreply.github.com>
2022-12-13 22:16:47 -0500

    Add files via upload
    Francis_Rodriguez_7997_V2

---
+++ PlayBox_7997-RODRIGUEZ_FRANCIS_V2/PlayBox_7997-master/FRANCIS_ALEJANDRO_RODRIGUEZ_CHICAIZA/Tetris/Position.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace Tetris
+{
+    public class Position
+    {
+        //Para representar un posición o celda en una cuadricula
+        //Se almacena una fila y una columna
+        public int Row { get; set; }
+        public int Column { get; set; }
+        //Agregar un construcor simple
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+
+        }
+    }
+}
